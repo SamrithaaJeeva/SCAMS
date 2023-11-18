97c477ff48eb93f66579734ba4dbe42463f0cedb
MadhunishaBala <madhunisha.20221334@iit.ac.lk>
2023-11-18 16:25:27 +0530

    Database connectivity added

---
+++ src/main/java/com/example/scams/Connector.java
@@ -0,0 +1,33 @@
+package com.example.scams;
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Connector {
+
+    final  static  String driver="com.mysql.cj.jdbc.Driver";
+    final  static String database_url="jdbc:mysql://localhost:3306/scams";
+    final static String username= "root";
+    final static String password="";
+    public static Connection  connection()
+    {
+        try
+        {
+            Class.forName(driver);
+            Connection c= DriverManager.getConnection(database_url,username,password);
+            System.out.println("Database is succesfully connected");
+            return c;
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null,e);
+            return null ;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        connection();
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.scams {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires java.sql;
 
 
     opens com.example.scams to javafx.fxml;
