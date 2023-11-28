a48aece47efa43fb3c14240250917c53e6c88d97
Samrithaaa <samrithaaj@gmail.com>
2023-11-28 12:36:25 +0530

    samritha commit 6

--- src/main/java/com/example/scams/viewclubs_Controller.java
+++ src/main/java/com/example/scams/viewclubs_Controller.java
@@ -1,5 +1,84 @@
 package com.example.scams;
 
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class viewclubs_Controller {
+    @FXML
+    private TableView<Clubs> clubtable;
+
+    @FXML
+    private TableColumn<Clubs, String> clubIDcolumn;
+
+    @FXML
+    private TableColumn<Clubs, String> clubnamecolumn;
+
+    @FXML
+    private TableColumn<Clubs, String> descriptioncolumn;
+
+    @FXML
+    private TableColumn<Clubs, String> advisornameolumn;
+
+
+    @FXML
+    private ObservableList<Clubs> data = FXCollections.observableArrayList();
+
+    private Connection con;  // Assuming con is initialized elsewhere in your code
+
+    public void Connect() {
+        con = Connector.connection();
+    }
+
+    @FXML
+    private void initialize() {
+        Connect();
+    }
+    @FXML
+    private void loadDataToTable(ActionEvent event) {
+        try {
+            // Clear existing data
+            data.clear();
+
+            PreparedStatement pat = con.prepareStatement("SELECT * FROM club");
+            ResultSet rs = pat.executeQuery();
+
+            while (rs.next()) {
+                Clubs clubs = new Clubs(
+                        rs.getString("Club_ID"),
+                        rs.getString("Club_Name"),
+                        rs.getString("ClubAdvisor_Name"),
+                        rs.getString("Club_Description"));
+
+                // Add the new data to the observable list
+                data.add(clubs);
+            }
+
+            // Set the items (data) to the table on the JavaFX Application Thread
+            Platform.runLater(() -> clubtable.setItems(data));
+
+            // Set the cell value factories for each column
+            clubIDcolumn.setCellValueFactory(new PropertyValueFactory<>("ClubID"));
+            clubnamecolumn.setCellValueFactory(new PropertyValueFactory<>("ClubName"));
+            advisornameolumn.setCellValueFactory(new PropertyValueFactory<>("AdvisorName"));
+            descriptioncolumn.setCellValueFactory(new PropertyValueFactory<>("Description"));
+
+        } catch (SQLException e) {
+            System.out.println("Error here");
+            e.printStackTrace();
+        }
+    }
+
+
 
 }
