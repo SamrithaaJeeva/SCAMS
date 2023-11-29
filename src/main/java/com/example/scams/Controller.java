84018430e3140686435dc6200b69a06347fae6ce
MadhunishaBala <madhunisha.20221334@iit.ac.lk>
2023-11-29 14:45:17 +0530

    Name

--- src/main/java/com/example/scams/Controller.java
+++ src/main/java/com/example/scams/Controller.java
@@ -12,7 +12,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Controller {
+public class Controller { //Madhunisha
     private Stage stage;
     private Scene scene;
     private Parent root;
